c31b0b9a83ea43639d1509dfea64a0682562eb76
3sf00r <153434346+3sf00r@users.noreply.github.com>
2024-11-30 09:47:52 -0800

    adding php and <main>

--- Assignment2.php
+++ Assignment2.php
@@ -6,12 +6,19 @@
     <link rel="stylesheet" href="https://unpkg.com/pico.css">
 </head>
 <body>
-    <header>UOB Nationality Table</header>
+    <main>
+        <?php
+            $url = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
+            $response = file_get_contents($url);
+            $data = json_decode($response, true);
+            ?>
 
-    <table>
-    <tr><th>Year</th><th>Semester</th><th>Program</th><th>Nationality</th><th>Number of Students</th></tr>
+        <header>UOB Nationality Table</header>
 
-        
-</table>
+        <table>
+            <tr><th>Year</th><th>Semester</th><th>Program</th><th>Nationality</th><th>Number of Students</th></tr>
+        </table>
+    </main>
+    
 </body>
 </html>
