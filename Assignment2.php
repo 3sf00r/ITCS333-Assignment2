07bd20b79dbd0736d566b7a0fb4be153db21d8c8
A-BH75 <amarzooq10@gmail.com>
2024-11-30 21:23:08 +0300

    linking secondary CSS

--- Assignment2.php
+++ Assignment2.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Assign_2 Stat Table</title>
     <link rel="stylesheet" href="https://unpkg.com/pico.css">
+    <link rel="stylesheet" href="styless.css">
 </head>
     
 <body>
