9ebfe735b25cc677274f98f3da30d6804c8142c7
A-BH75 <amarzooq10@gmail.com>
2024-11-30 20:51:46 +0300

    reconfiguring the table & adding id set

--- Assignment2.php
+++ Assignment2.php
@@ -11,13 +11,17 @@
             $url = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
             $response = file_get_contents($url);
             $data = json_decode($response, true);
+            if ($data && isset($data['results'])) {
+                echo '<table>';
+                foreach ($data['results'] as $result) {
+                    echo '<tr>';
+                    echo '<td>' . $result['year'] . '</td>';
+                    echo '</tr>';
+                }
             ?>
 
         <header>UOB Nationality Table</header>
 
-        <table>
-            <tr><th>Year</th><th>Semester</th><th>Program</th><th>Nationality</th><th>Number of Students</th></tr>
-        </table>
     </main>
     
 </body>
