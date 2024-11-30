825581fe31b1fc518c9c8010389d2119f51179ba
A-BH75 <amarzooq10@gmail.com>
2024-11-30 21:35:10 +0300

    adding comments

--- Assignment2.php
+++ Assignment2.php
@@ -10,15 +10,19 @@
     
 <body>
     <main>
-        <?php
+        <?php 
+
+            // Fetch data from the API
             $url = 'https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100';
             $response = file_get_contents($url);
             $data = json_decode($response, true);
+
+            // Display the data in a table
             if ($data && isset($data['results'])) {
                 echo '<header>UOB Nationality Table</header>';
                 echo '<table>';
                 echo '<tr><th>Year</th><th>Semester</th><th>Program</th><th>Nationality</th><th>Number of Students</th></tr>';
-                
+                //Display data from the API
                 foreach ($data['results'] as $result) {
                     echo '<tr>';
                     echo '<td>' . $result['year'] . '</td>';
@@ -30,6 +34,8 @@
                 }
                 echo '</table>';
                 }
+
+            // Display error message if data retrieval fails    
             else { 
                 echo 'Error retrieving data'; 
             }
